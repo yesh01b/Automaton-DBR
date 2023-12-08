2e83e2e12c1cadfc6e7b2a131c75fb0887860b91
yeswanth <yeswanthb40@gmail.com>
2023-12-08 13:55:53 +0530

    updated ps1 script

--- CICD/scripts/DatabricksToken.ps1
+++ CICD/scripts/DatabricksToken.ps1
@@ -2,11 +2,20 @@ param
 {
     [parameter(Mandatory = $true)] [string] $databricksWorkspaceResourceId,
     [parameter(Mandatory = $true)] [string] $databricksWorkspaceUrl,
-    [parameter(Mandatory = $false)] [int] $tokenLifeTimeSecods = 300
+    [parameter(Mandatory = $false)] [int] $tokenLifeTimeSeconds = 300
 }
 
 $azureDatabricksPricipalId = ''
 
 $headers = @{}
 $headers["Authorization"] = "Bearer $((az account get-access-token --resource $azureDatabricksPricipalId | convertFrom-Json).accessToken)"
-$headers["X-Databricks-Azure-SP-Management-Token"] = "$"
+$headers["X-Databricks-Azure-SP-Management-Token"] = "$((az account get-access-token --resource https://management.core.widows.net/ ))"
+$headers["X-Databricks-Azure-Workspace-Resource-Id"] = $databricksWorkspaceResourceId
+
+$json = @{}
+$json["Lifetime_seconds"] = $tokenLifeTimeSeconds
+
+$req = Invoke-WebRequest -Uri "https://$databricksWorkspaceUrl/api/2.0/token/create" -Body ($json | ConvertTo-Json) -ContentType "application"
+$bearerToken = ($req.content | ConvertFrom-Json).token_value
+
+return $bearerToken
